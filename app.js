6de9ffe10831401a5123a51cf8d1c9eb1f94c2bf
Drew LeSueur <drewalex@gmail.com>
2014-07-04 22:02:43 +0000

    comment out something I copy pasted

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express")
 var app = express()
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+//app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({extended: true}) );
 
 // handle incoming POST from twilio
